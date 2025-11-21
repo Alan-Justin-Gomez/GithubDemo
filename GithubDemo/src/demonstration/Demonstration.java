596b740d049e350ef3cf8bb9a574708ea6770c54
Alan Justin Gomez <ajustin@lakeheadu.ca>
2025-11-21 11:43:42 -0500

    stage2

--- GithubDemo/src/demonstration/Demonstration.java
+++ GithubDemo/src/demonstration/Demonstration.java
@@ -4,6 +4,7 @@ public class Demonstration {
 
 	public static void main(String[] args) {
 		System.out.print("Hello World This is a demonstration");
+		System.out.print("Hello World This is a demonstration2");
 	}
 
 }
