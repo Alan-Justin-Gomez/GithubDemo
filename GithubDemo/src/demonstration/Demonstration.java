4f619a85d94788324c3f664901093b6abbcc3aa0
Alan Justin Gomez <ajustin@lakeheadu.ca>
2025-11-21 11:42:14 -0500

    Update print statement in Demonstration.java

--- GithubDemo/src/demonstration/Demonstration.java
+++ GithubDemo/src/demonstration/Demonstration.java
@@ -3,7 +3,7 @@ package demonstration;
 public class Demonstration {
 
 	public static void main(String[] args) {
-		System.out.print("Hello World");
+		System.out.print("Hello World This is a demonstration");
 	}
 
 }
